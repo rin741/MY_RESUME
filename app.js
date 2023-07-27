9b73eacb2ed77688834f9846fbcc39af957063f9
rimi <gab.ceballosp@gmail.com>
2023-07-27 07:59:05 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
